cbd1e7922d16fb2cc6f1e78bd6213a0f39a7620e
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-06-24 12:01:09 -0400

    Now at the beginning of the 5th tutorial. Learning how to create automated tests in django

--- DjangoRitesOfPassage/ThouShallPass/polls/urls.py
+++ DjangoRitesOfPassage/ThouShallPass/polls/urls.py
@@ -10,11 +10,20 @@ from polls import views
 #create a URLconf so that Django can call a view by mapping it to a URL
 urlpatterns = patterns('', 
 	#ex: /polls/
-	url(r'^$', views.index, name='index'),
+	#url(r'^$', views.index, name='index'),
+	#This is the simpler updated version "Generic Views Version"
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	
 	#ex: /polls/5/
-	url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	#url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	#This is the "Generic Views Version"
+	url(r'^(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
+	
 	#ex: /poll/5/results/
-	url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+	#url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+	#This is the "Generic Views Version"
+	url(r'^(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+	
 	#ex: /polls/5/vote/
 	url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
